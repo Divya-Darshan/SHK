4f23faf5f471405b2ca411eccada98b73b853069
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-07-31 23:04:01 +0530

    caro img

--- src/comp/carousel/caro.jsx
+++ src/comp/carousel/caro.jsx
@@ -3,17 +3,25 @@ import './caro.css';
 function Caro() {
   return (
     <>
-      <section className="caro">
-        <div>
-          <img src="https://d3ffkb7uf3bq3r.cloudfront.net/collection/848f7828-88e0-4668-b673-e4a497f2133f/mobile_hero_image-c94731aa-1618-4d91-aad1-7ed649b056d7-original.jpg?v=63919292021&width=600" alt="Image 1" />
-        </div>
-        <div>
-          <img src="https://d3ffkb7uf3bq3r.cloudfront.net/collection/656d44e2-270c-481c-b202-a51980dabcf8/mobile_hero_image-6c767366-d339-468b-b2dc-bf3b65719e4b-original.jpg?v=63918535971&width=600" alt="Image 2" />
-        </div>
-        <div>
-          <img src="https://d3ffkb7uf3bq3r.cloudfront.net/collection/656d44e2-270c-481c-b202-a51980dabcf8/mobile_hero_image-6c767366-d339-468b-b2dc-bf3b65719e4b-original.jpg?v=63918535971&width=600" alt="Image 3" />
-        </div>
-      </section>
+    <section>
+        <span className='lable'>Shop Our Collections <i class="fa-solid fa-wand-magic-sparkles"></i></span>
+      <dev className="caro">
+            <div className='img-container'>
+                <img src="./img/mens.jpg" alt="Image 1" />
+                    <div>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, recusandae ea voluptatibus labore, nobis non libero fugiat voluptatem sequi iusto repudiandae perspiciatis facilis repellat quaerat doloremque quasi adipisci dolorem molestias.
+                        </p>
+                    </div>
+            </div>
+            <div className='img-container'>
+                <img src="./img/womens.jpg" alt="Image 2" />
+            </div>
+            <div className='img-container'>
+                <img src="./img/kids.jpg" alt="Image 3" />        
+            </div>
+      </dev>
+    </section>
     </>
   );
 }
