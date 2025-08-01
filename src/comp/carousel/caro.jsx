d8b80e927ac605a52c6b0d81e938ade671028046
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-08-01 13:00:45 +0530

    res Gride comp 📱

--- src/comp/carousel/caro.jsx
+++ src/comp/carousel/caro.jsx
@@ -3,25 +3,37 @@ import './caro.css';
 function Caro() {
   return (
     <>
-    <section>
-        <span className='lable'>Shop Our Collections <i class="fa-solid fa-wand-magic-sparkles"></i></span>
-      <dev className="caro">
-            <div className='img-container'>
-                <img src="./img/mens.jpg" alt="Image 1" />
-                    <div>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, recusandae ea voluptatibus labore, nobis non libero fugiat voluptatem sequi iusto repudiandae perspiciatis facilis repellat quaerat doloremque quasi adipisci dolorem molestias.
-                        </p>
-                    </div>
+      <section>
+        <span className='lable'>
+          Shop Our Collections <i className="fa-solid fa-wand-magic-sparkles"></i>
+        </span>
+        <div className="caro">
+          <div className='img-container'>
+            <img src="./img/mens.jpg" alt="Men's Collection" />
+            <div>
+              <p>
+                Explore stylish and comfortable menswear designed for everyday elegance.
+              </p>
             </div>
-            <div className='img-container'>
-                <img src="./img/womens.jpg" alt="Image 2" />
+          </div>
+          <div className='img-container'>
+            <img src="./img/womens.jpg" alt="Women's Collection" />
+            <div>
+              <p>
+                Discover fashion-forward outfits perfect for every occasion and mood.
+              </p>
             </div>
-            <div className='img-container'>
-                <img src="./img/kids.jpg" alt="Image 3" />        
+          </div>
+          <div className='img-container'>
+            <img src="./img/kids.jpg" alt="Kids' Collection" />
+            <div>
+              <p>
+                Bright, playful, and cozy styles for your little ones to shine.
+              </p>
             </div>
-      </dev>
-    </section>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/comp/grid/grid.jsx
+++ src/comp/grid/grid.jsx
@@ -7,25 +7,24 @@ function Grid() {
     { id: 3, name: "Red Jacket", price: "", src: "https://d3ffkb7uf3bq3r.cloudfront.net/image/42dc333b-427c-4f3a-ac02-733686b70f90/image-6c3f215a-8978-4d04-be21-3b154e6707f6-original.png?v=63890796739&width=1024" },
     { id: 4, name: "Blue Shirt", price: "", src: "https://d3ffkb7uf3bq3r.cloudfront.net/image/22dfa559-6d3b-4123-bc28-45df6ffea2e6/image-c105566e-1608-4eef-b39e-335dc718dbd9-original.png?v=63890795886&width=1024" },
     { id: 5, name: "Green Polo", price: "", src: "https://d3ffkb7uf3bq3r.cloudfront.net/image/09b6f912-d04a-465b-a249-c3fa4ae2a58a/image-6ee3fad8-f199-45b7-9868-2797db8b76ea-original.png?v=63890793708&width=1024" },
-    { id: 6, name: "Grey Sweatshirt", price: "", src: "./shirt.jpg" },    
-  ];    
+    { id: 6, name: "Grey Sweatshirt", price: "", src: "./shirt.jpg" },
+  ];
 
   return (
     <>
-    <section className='products' >
-      <div className='lable'>For You<i class="fa fa-level-up" aria-hidden="true"></i></div>
-      <div className="product-grid-container">
-        {products.map((product) => (
-          <div className="product-card" key={product.id}>
-            <img className="pro1" src={product.src} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p className="price">{product.price}</p>
-            <button className="btn">Add to Cart</button>
-          </div>
-        ))}
-      </div>
-    </section>
-    
+      <div className="lable">For You <i className="fa fa-level-up" aria-hidden="true"></i></div>
+      <section className="products">
+        <div className="product-grid-container">
+          {products.map((product) => (
+            <div className="product-card" key={product.id}>
+              <img className="pro1" src={product.src} alt={product.name} />
+              <h3>{product.name}</h3>
+              <p className="price">{product.price}</p>
+              <button className="btn">Add to Cart</button>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
