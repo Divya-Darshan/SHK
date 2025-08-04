1d1f32e0fac4dbab9cc182b12a6a26eb2d85da1e
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-08-04 21:26:16 +0530

    ifroma location redesign📍

--- src/comp/foot/foot.jsx
+++ src/comp/foot/foot.jsx
@@ -7,6 +7,13 @@ function Foot(){
     return(
         <>
             <footer>
+                <h3>Our Location <i className="ri-map-pin-line" /></h3>
+                <section className='iframe-container'>
+
+                    <iframe className='loco' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3884.5570632162044!2d80.18414107604575!3d13.190304210043742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a527ca06318406f%3A0x6172bd3651734ff0!2sSri%20Harikrishna%20Textiles!5e0!3m2!1sen!2sin!4v1754321136996!5m2!1sen!2sin' />
+                        
+                </section>
+
                 <section className='foot-container'>
                     <div className='foot-card'>
                         
@@ -55,11 +62,7 @@ function Foot(){
 
                 </section>
 
-                <section>
 
-                    <iframe className='loco' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3884.5570632162044!2d80.18414107604575!3d13.190304210043742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a527ca06318406f%3A0x6172bd3651734ff0!2sSri%20Harikrishna%20Textiles!5e0!3m2!1sen!2sin!4v1754321136996!5m2!1sen!2sin' />
-                        
-                </section>
             </footer>
 
         </>
