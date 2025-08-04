3f0dd9936a136412a554ef73fa77a292f9f95fe1
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-08-04 18:43:45 +0530

    for u grid rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Nav from './comp/nav/nav'
-import Grid from './comp/grid/grid'
+import Grid from './comp/grid/ForU-grid'
 import Caro from './comp/carousel/caro'
 import Foot from './comp/foot/foot'
 function App() {
