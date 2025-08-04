d1a54c6aee7b69f0f1e79d19ba1cefaa324f2435
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-08-04 20:37:42 +0530

    add res img(^///^)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Nav from './comp/nav/nav'
-import Grid from './comp/grid/ForU-grid'
+import ForUGrid from './comp/grid/ForU-grid'
 import Caro from './comp/carousel/caro'
 import Foot from './comp/foot/foot'
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <Nav/>
-    <Grid/>
+    <ForUGrid/>
     <Caro/>
     <Foot/>
     </>
